f890137195194773e14931f05789c94288363d22
sokelova <sokelova@gmail.com>
2021-07-26 00:25:38 +0300

    commit message

---
+++ src/Input.tsx
@@ -0,0 +1,9 @@
+// import React from "react";
+//
+// const Input = () => {
+//
+//     return <div>
+//         <input value={title} onChange={onChangeHandler} onKeyPress={onKeyPressHandler}/>
+//         <button onClick={addTask}>+</button>
+//     </div>
+// }
